44ab80be3fe8165e1ae0980768acf4840da32132
Daud <daudisam007@gmail.com>
2025-04-05 16:44:37 +0500

    tel values

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -5,14 +5,14 @@ function Pool() {
     <div className="container">
       <h1>Select the pool to join lucky draw</h1>
       <div className="card-container">
-        <a href="tel:*786#" className="card">
+        <a href="tel:*786*1*1*1*03044519969*10#" className="card">
+          <h2>10rs</h2>
+        </a>
+        <a href="tel:*tel:*786*1*1*1*03044519969*50#" className="card">
+          <h2>50rs</h2>
+        </a>
+        <a href="tel:*786*1*1*1*03044519969*100#" className="card">
           <h2>100rs</h2>
-        </a>
-        <a href="tel:*786#" className="card">
-          <h2>500rs</h2>
-        </a>
-        <a href="tel:*786#" className="card">
-          <h2>1000rs</h2>
         </a>
       </div>
     </div>
